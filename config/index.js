82a9cbb7c4a0b8afb90b926abac992c2ed8ac05d
fanhehe <13889441200@163.com>
2016-10-23 11:50:18 +0800

    update .gitignore and modify the config info

--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
+var env = process.env.NODE_ENV;
 var config = require('./deploy.json');
 
 if (fs.existsSync('./local.json')) {
     config = require('./local.json');
 }
 
-module.exports = config;
+module.exports = config[env];
