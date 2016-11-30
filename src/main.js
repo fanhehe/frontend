394c08603eac1f759475e0591c12a62e8751d852
fanhehe <13889441200@163.com>
2016-11-30 15:30:57 +0800

    delete global routes config

--- src/main.js
+++ src/main.js
@@ -3,19 +3,16 @@ import VueRouter from 'vue-router';
 
 import Routes from './routes/';
 import Store from './vuex/store';
-import RouterConfig from './routes/RouterConfig';
 
 Vue.use(VueRouter);
 
 const store = Store;
 const routes = Routes;
 
-let router = new VueRouter({
+const router = new VueRouter({
 	mode: 'hash',
 	routes: routes,
 });
-
-router = RouterConfig(router);
 
 if (module.hot) {
 	module.hot.accept();
